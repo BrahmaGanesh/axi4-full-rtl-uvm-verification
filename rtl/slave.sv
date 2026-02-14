b473791c8264e0575b263f990b1d1f42ffb348e3
BrahmaGanesh <katrapallibrahmaganesh@gmail.com>
2026-02-14 23:11:30 +0530

    AXI slave: fix WREADY/RLAST handling and improve error checks

--- rtl/slave.sv
+++ rtl/slave.sv
@@ -33,8 +33,8 @@ module axi_slave #(
        output    logic [DATA_WIDTH - 1 :0]   RDATA,
        output    logic                       RLAST,
        output    logic                       RVALID,
-       input   logic                       RREADY,
-       output   logic [1:0]                 RRESP
+       input     logic                       RREADY,
+       output   logic [1:0]                  RRESP
     );
 
     logic [DATA_WIDTH - 1 :0] mem [0:DEPTH - 1];
@@ -46,16 +46,20 @@ module axi_slave #(
     logic [1:0] wr_burst_reg, rd_burst_reg;
     logic [7:0] wr_beat_cnt, rd_beat_cnt;
     logic [ADDR_WIDTH - 1 :0] wr_wrap_boundary_reg, rd_wrap_boundary_reg;
+  	
+  	logic wr_error_flag, rd_error_flag;
 
     typedef enum logic [1:0] { 
         W_IDLE,
         W_TRANSFER,
+      	W_ERROR,
         B_RESP
         } wr_state_t;
     
     typedef enum logic [1:0] {
         R_IDLE,
-        R_TRANSFER
+        R_TRANSFER,
+      	R_ERROR
         } rd_state_t;
     
     wr_state_t wr_state;
@@ -98,48 +102,87 @@ module axi_slave #(
 
     always_ff @(posedge ACLK or negedge ARESETn) begin
         if(!ARESETn) begin
-            wr_state <= W_IDLE;
-            AWREADY <= 0;
-            WREADY  <= 0;
-            BVALID  <= 0;
-            wr_len_reg <= 0;
-            wr_size_reg <= 0;
-            wr_burst_reg <=0;
-            wr_beat_cnt <= 0;
-            wr_addr_reg <= 0;
+            wr_state        <= W_IDLE;
+            AWREADY         <= 0;
+            WREADY          <= 0;
+            BVALID          <= 0;
+          	BRESP           <= 2'b00;
+            wr_len_reg      <= 0;
+            wr_size_reg     <= 0;
+            wr_burst_reg    <=0;
+            wr_beat_cnt     <= 0;
+            wr_addr_reg     <= 0;
             wr_next_addr_reg <= 0;
         end
         else begin
+            AWREADY <= 0;
+            WREADY  <= 0;
+            BRESP   <= BRESP;
+            BVALID  <= BVALID;
 
             case(wr_state)
-                W_IDLE        :   begin
-                                    AWREADY <= 1;
-                                    WREADY  <= 0;
-                                    BVALID  <= 0;
-                                    if(AWVALID && AWREADY) begin
-                                        wr_addr_reg <= AWADDR;
-                                        wr_next_addr_reg <= AWADDR;
-                                        wr_burst_reg <= AWBURST;
-                                        wr_len_reg <= AWLEN;
-                                        wr_size_reg <= AWSIZE;
-                                        wr_beat_cnt <= AWLEN + 1;
-                                        wr_wrap_boundary_reg <= wrap_boundary(AWADDR,AWLEN,AWSIZE);
+                W_IDLE          :   begin
+                                        AWREADY <= 1;
+                                        WREADY  <= 0;
+                                        BVALID  <= 0;
+                                        wr_error_flag = 0;
 
-                                        AWREADY <= 0;
-                                        WREADY  <= 1;
-                                        wr_state <= W_TRANSFER;
+                                        if(AWVALID && AWREADY) begin
+
+                                          if(AWADDR % (1 << AWSIZE) != 0) 
+                                            wr_error_flag = 1;
+                                            if (AWBURST == 2'b11) wr_error_flag = 1;
+                                            if ((AWADDR[11:0] + ((AWLEN+1) << AWSIZE)) > 12'hFFF) begin
+                                                BRESP   <= 2'b10;
+                                                BVALID  <= 1;
+                                                AWREADY <= 0;
+                                                WREADY  <= 0;
+                                                wr_state <= W_ERROR;
+                                            end
+                                            if ((AWADDR >> $clog2(DATA_WIDTH/8)) >= DEPTH) begin
+                                                BRESP   <= 2'b10;
+                                                BVALID  <= 1;
+                                                AWREADY <= 0;
+                                                WREADY  <= 0;
+                                                wr_error_flag <= 1;
+                                                wr_state <= W_ERROR;
+                                            end
+                                            if (AWSIZE > $clog2(DATA_WIDTH)) wr_error_flag = 1;
+                                            if(!wr_error_flag)begin
+                                            
+                                                wr_addr_reg <= AWADDR;
+                                                wr_next_addr_reg <= AWADDR;
+                                                wr_burst_reg    <= AWBURST;
+                                                wr_len_reg      <= AWLEN;
+                                                wr_size_reg     <= AWSIZE;
+                                                wr_beat_cnt     <= AWLEN + 1;
+                                                wr_wrap_boundary_reg <= wrap_boundary(AWADDR,AWLEN,AWSIZE);
+
+                                                AWREADY <= 0;
+                                                WREADY  <= 1;
+                                                wr_state <= W_TRANSFER;
+                                            end
+                                            else begin
+                                                BRESP   <= 2'b10;
+                                                BVALID  <= 1;
+                                                AWREADY <= 0;
+                                                WREADY  <= 0;
+                                                wr_state <= W_ERROR;
+                                            end
+                                        end
                                     end
-                                end
                 W_TRANSFER   :   begin
+                   WREADY <= 1;
                                     if(WVALID && WREADY) begin
                                        
                                         for(int i=0; i< DATA_WIDTH/8; i++) begin
+
                                             if(WSTRB[i])
                                                 mem[wr_next_addr_reg >> $clog2(DATA_WIDTH/8)][8*i +: 8] <= WDATA[8*i +: 8];
-
                                         end
                                         wr_next_addr_reg <= next_addr(wr_next_addr_reg, wr_burst_reg, wr_size_reg, wr_wrap_boundary_reg, wr_len_reg);
                                         wr_beat_cnt <= wr_beat_cnt - 1;
+                                        
                                         if(WLAST && wr_beat_cnt == 1)begin
                                             BRESP       <= 2'b00;
                                             WREADY      <= 0;
@@ -160,6 +203,13 @@ module axi_slave #(
                                         end
                                     end
                                 end
+              W_ERROR		: 	begin
+                					WREADY <= 0;
+                                    if(BVALID && BREADY) begin
+                  	                    BVALID <= 0;
+                	                    wr_state <= W_IDLE;
+                                    end
+              					end
                 B_RESP      :   begin
                                     if(BVALID && BREADY) begin
                                         wr_state    <= W_IDLE;
@@ -175,26 +225,59 @@ module axi_slave #(
             rd_state        <= R_IDLE;
             ARREADY         <= 0;
             RVALID          <= 0;
+          	RRESP 			<= 2'b00;
+          	RLAST			<= 0;
             rd_len_reg      <= 0;
             rd_size_reg     <= 0;
             rd_burst_reg    <=0;
             rd_beat_cnt     <= 0;
             rd_addr_reg     <= 0;
+          	rd_error_flag	<= 0;
         end
         else begin
+            ARREADY <= 0;
+            RVALID  <= RVALID;
+            RRESP   <= RRESP;
+            RLAST   <= 0;
+
             case(rd_state)
                 R_IDLE      :   begin
                                     ARREADY <= 1;
+                                    rd_error_flag	<= 0;
                                     if( ARVALID && ARREADY) begin
-                                        rd_addr_reg <= ARADDR;
-                                        rd_len_reg  <= ARLEN;
-                                        rd_size_reg <= ARSIZE;
-                                        rd_burst_reg <= ARBURST;
-                                        rd_beat_cnt <= ARLEN + 1;
-                                        rd_wrap_boundary_reg <= wrap_boundary(ARADDR, ARLEN, ARSIZE);
+                                        if (ARADDR % (1 << ARSIZE) != 0) rd_error_flag = 1;
+                                        if (ARBURST == 2'b11) rd_error_flag = 1;
+                                        if ((ARADDR[11:0] + ((ARLEN+1) << ARSIZE)) > 12'hFFF) begin
+                                            RRESP   <= 2'b10; 
+                                            RVALID  <= 1;
+                                            RLAST   <= 1;
+                                            ARREADY <= 0;
+                                            rd_state <= R_ERROR;
+                                        end
+                                        if ((ARADDR >> $clog2(DATA_WIDTH/8)) >= DEPTH) begin
+                                            RRESP   <= 2'b10;
+                                            RVALID  <= 1;
+                                            ARREADY <= 0;
+                                            rd_state <= R_ERROR;
+                                        end
+                                        if (ARSIZE > $clog2(DATA_WIDTH)) rd_error_flag = 1;
+                                        if(!rd_error_flag) begin
+                                            rd_addr_reg     <= ARADDR;
+                                            rd_len_reg      <= ARLEN;
+                                            rd_size_reg     <= ARSIZE;
+                                            rd_burst_reg    <= ARBURST;
+                                            rd_beat_cnt     <= ARLEN + 1;
+                                            rd_wrap_boundary_reg <= wrap_boundary(ARADDR, ARLEN, ARSIZE);
 
-                                        RVALID      <= 1;
-                                        rd_state    <= R_TRANSFER;
+                                            RVALID      <= 1;
+                                            rd_state    <= R_TRANSFER;
+                                        end
+                                        else begin
+                                            RRESP       <= 2'b10;
+                                            RVALID      <= 1;
+                                            ARREADY     <= 0;
+                                            rd_state    <= R_ERROR;
+                                        end
                                     end
                                 end
                 R_TRANSFER  :   begin
@@ -204,17 +287,27 @@ module axi_slave #(
                                     if(RVALID && RREADY) begin
                                         rd_addr_reg <= next_addr(rd_addr_reg, rd_burst_reg, rd_size_reg, rd_wrap_boundary_reg, rd_len_reg); 
                                         rd_beat_cnt <= rd_beat_cnt - 1;
+                                        RLAST   <= (rd_beat_cnt == 1);
                                     end
 
-                                    RLAST   <= (rd_beat_cnt == 1) && RVALID;
+                                    
                                     if(RVALID && RREADY && RLAST) begin
                                         rd_state    <= R_IDLE;
                                         RVALID      <= 0;
                                         RRESP       <= 2'b00;
                                     end
                                 end
+              R_ERROR		:	begin
+              					    if(RVALID && RREADY) begin
+      									rd_beat_cnt <= rd_beat_cnt - 1;
+      									RLAST <= (rd_beat_cnt == 1);
+      									if(RLAST) begin
+         									RVALID <= 0;
+         									rd_state <= R_IDLE;
+      									end
+                                    end
+              					end
             endcase
         end
     end
-    
 endmodule
