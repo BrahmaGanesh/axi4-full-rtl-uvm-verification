d783af55524c174ae42547dfe1f2fec7bb32931d
BrahmaGanesh <katrapallibrahmaganesh@gmail.com>
2026-02-20 23:19:55 +0530

    [axi4_if] Added protocol assertions and cover properties

--- tb/interfacce/axi4_if.sv
+++ tb/interfacce/axi4_if.sv
@@ -2,11 +2,12 @@
 // Project     : AXI4 UVM VERIFICATION
 // File        : axi4_if.sv
 // Author      : Brahma Ganesh Katrapalli
-// Date        : 14-02-2026
-// Version     : 1.0
-// Description : AXI4 interface definition for connecting
-//               master and slave modules, including
-//               all channel signals (AW, W, B, AR, R)
+// Date        : 20-02-2026
+// Version     : 1.1
+// Description : AXI4 interface with protocol assertions
+//               and cover properties for handshake,
+//               burst alignment, response validity,
+//               and address alignment.
 //=====================================================
 
 interface axi4_interface #(
@@ -45,5 +46,48 @@ interface axi4_interface #(
     logic                        RREADY;
     logic                        RVALID;
     logic   [1:0]                RRESP;
+    logic 	[7:0]				 beat_cnt;
+  
+  
+
+    property aw_handshake;
+        @(posedge ACLK) disable iff (!ARESETn)
+        AWVALID |-> AWREADY;
+    endproperty
+    assert_aw_handshake: assert property (aw_handshake)
+    else $error("AW channel handshake failed");
+    cover property (aw_handshake);
+
+    property wlast_check;
+        @(posedge ACLK) disable iff (!ARESETn)
+        (WVALID && WREADY && WLAST) |-> (beat_cnt  == AWLEN);
+    endproperty
+    assert_wlast_check: assert property (wlast_check)
+    else $error("WLAST not aligned with AWLEN");
+    cover property (wlast_check);
+
+    property bresp_valid;
+        @(posedge ACLK) disable iff (!ARESETn)
+        BVALID |-> (BRESP inside {2'b00, 2'b01, 2'b10, 2'b11});
+    endproperty
+    assert_bresp_valid: assert property (bresp_valid)
+    else $error("Invalid BRESP value detected");
+    cover property (bresp_valid);
+
+    property rlast_check;
+        @(posedge ACLK) disable iff (!ARESETn)
+        (RVALID && RREADY && RLAST) |-> (beat_cnt == ARLEN);
+    endproperty
+    assert_rlast_check: assert property (rlast_check)
+    else $error("RLAST not aligned with ARLEN");
+    cover property (rlast_check);
+
+    property addr_alignment;
+        @(posedge ACLK) disable iff (!ARESETn)
+        AWVALID |-> (AWADDR % (1 << AWSIZE) == 0);
+    endproperty
+    assert_addr_alignment: assert property (addr_alignment)
+    else $error("AWADDR not aligned to AWSIZE");
+    cover property (addr_alignment);
 
 endinterface
