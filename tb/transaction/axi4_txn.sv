6dc423f29a7ee9718a4da6e9bf43e7032e02ce38
BrahmaGanesh <katrapallibrahmaganesh@gmail.com>
2026-02-20 22:38:36 +0530

    [axi4_txn] Add last_RRESP field and refine memory depth constraint

--- tb/transaction/axi4_txn.sv
+++ tb/transaction/axi4_txn.sv
@@ -2,8 +2,8 @@
 // Project     : AXI4 UVM VERIFICATION
 // File        : axi4_txn.sv
 // Author      : Brahma Ganesh Katrapalli
-// Date        : 18-02-2026
-// Version     : 1.1
+// Date        : 20-02-2026
+// Version     : 1.2
 // Description : UVM sequence item representing AXI4
 //               transactions across AW, W, B, AR, R
 //               channels. Updated enum definition and
@@ -23,21 +23,24 @@ class axi4_transaction extends uvm_sequence_item;
 	rand bit [1:0]  AWBURST;
   
   	rand bit [31:0] WDATA;
-    rand bit [3:0]  WSTRB;
+  	rand bit [3:0]  WSTRB;
     rand bit        WLAST;
   
   	rand bit [31:0] ARADDR; 
 	rand bit [7:0]  ARLEN;
 	rand bit [2:0]  ARSIZE;
 	rand bit [1:0]  ARBURST;
+  
+         bit [31:0] wdata [0:255];
+         bit [31:0] wstrb [0:255];
 
-    	 bit [31:0] RDATA;
-         bit [1:0]  RRESP;
+         bit [31:0] RDATA [0:255];
+         bit [1:0]  RRESP[0:255];
          bit        RLAST;
   		
   		 bit [1:0]  BRESP;
+  		 bit [1:0] last_RRESP;
 
-        
          bit        AWVALID;
          bit        AWREADY;
          bit        WVALID;
@@ -59,16 +62,15 @@ class axi4_transaction extends uvm_sequence_item;
         soft AWADDR inside {[32'h0000_0000 : 32'h0000_FFFF]};
         }
     constraint alignment_and_boundary_c {
-        AWADDR % (1 << AWSIZE) == 0;
+      AWADDR % ((AWLEN+1) * (1 << AWSIZE)) == 0;
         (AWADDR[11:0] + ((AWLEN + 1) << AWSIZE)) <= 4090;
         }
     constraint memory_depth_c {
         soft (AWADDR >> $clog2(32/8)) < 256;
         }
 
-
     `uvm_object_utils_begin(axi4_transaction)
-         `uvm_field_enum(operation_t, op, UVM_ALL_ON)
+        `uvm_field_enum(operation_t, op, UVM_ALL_ON)
         `uvm_field_int(AWADDR, UVM_ALL_ON)
         `uvm_field_int(AWLEN, UVM_ALL_ON)
         `uvm_field_int(AWSIZE, UVM_ALL_ON)
@@ -81,9 +83,7 @@ class axi4_transaction extends uvm_sequence_item;
         `uvm_field_int(ARLEN, UVM_ALL_ON)
         `uvm_field_int(ARSIZE, UVM_ALL_ON)
         `uvm_field_int(ARBURST, UVM_ALL_ON)
-        `uvm_field_int(RDATA, UVM_ALL_ON)
         `uvm_field_int(RLAST, UVM_ALL_ON)
-        `uvm_field_int(RRESP, UVM_ALL_ON)
     `uvm_object_utils_end
 
     function new( string name = "axi4_transaction");
